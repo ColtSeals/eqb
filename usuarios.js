2e28314511091e3c2cd6ce1222223c0c2caa72e3
ColtSeals <colt_pfa@hotmail.com>
2023-09-16 23:01:22 -0300

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,6 @@
+// usuarios.js
+var users = [
+    { username: "152846", password: "luanque" },
+    { username: "usuario2", password: "senha2" },
+    // Adicione mais usuários conforme necessário
+];
