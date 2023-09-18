19d2b82432d945cb604c570d60be7c8e522d5e64
ColtSeals <colt_pfa@hotmail.com>
2023-09-17 22:49:45 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,6 +1,47 @@
-// usuarios.js
+// usuarios.js 
+
 var users = [
-    { username: "152846", password: "luanque" },
-    { username: "usuario2", password: "senha2" },
-    // Adicione mais usuários conforme necessário
+
+    { username: "152846", password: "luanque" },
+
+    { username: "118374", password: "118374" },
+
+    { username: "146919", password: "146919" },
+
+    { username: "120633", password: "120633" },
+
+    { username: "962988", password: "962988" },
+
+    { username: "126776", password: "126776" },
+
+    { username: "120065", password: "120065" },
+
+    { username: "981146", password: "981146" },
+
+    { username: "990204", password: "990204" },
+
+    { username: "124792", password: "124792" },
+
+    { username: "973807", password: "973807" },
+
+    { username: "129432", password: "129432" },
+
+    { username: "964007", password: "964007" },
+
+    { username: "127936", password: "127936" },
+
+    { username: "107496", password: "107496" },
+
+    { username: "105763", password: "105763" },
+
+    { username: "126072", password: "126072" },
+
+    { username: "923011", password: "923011" },
+
+    { username: "124907", password: "124907" },
+
+    { username: "118687", password: "118687" },
+
+    // Adicione mais usuários conforme necessário
+
 ];
