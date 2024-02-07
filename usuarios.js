612d23405a76a18c58b28c56c58a68ec31b8717b
ColtSeals <colt_pfa@hotmail.com>
2024-02-07 07:58:34 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -4,19 +4,24 @@ var users = [
 
 // EQUIPE B VDM
 
-
+    { username: "153026", password: "frederico" },
+  
+    { username: "143021", password: "110207" },
+  
+    { username: "usuario", password: "senha" },
+  
+    { username: "101870", password: "170406" },
+  
+    { username: "147502", password: "147502" },
 
     { username: "152846", password: "luanque" },  // CB PM LUANQUE
 
     { username: "148724", password: "148724" },  // CB PM JEFFERSON
 
-    { username: "101870", password: "101870" },  // CB PM SCARABELLO
+    { username: "101870", password: "170406" },  // CB PM SCARABELLO
 
     { username: "106864", password: "106864" },  // SD PM FREDERICO
 
-    { username: "973641", password: "973641" },  // SD PM THIAGO
-
-    { username: "100949", password: "100949" },  // SGT-PM ANA CLAUDIA
 
 
 ];
